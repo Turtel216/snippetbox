0d0971f9dd7837c4acc9f663ed2afbeb495e8857
turtel42 <papadimitrios216@gmail.com>
2024-04-09 01:00:58 +0300

    added depentency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
   "os"
 )
 
+type application struct {
+  errorLog  *log.Logger
+  infoLog   *log.Logger
+}
+
 func main() {
   
   // You can run go run ./cmd/web -help to get the flag info e.g. "HTTP network adress" and the conversion type e.g. String
@@ -18,24 +23,28 @@ func main() {
   // go run ./cmd/web >>/tmp/info.log 2 >>/tmp/error.log
 
   infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
   errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Lshortfile)
+
+  app := &application{
+    errorLog: errorLog,
+    infoLog:  infoLog,
+  }
 
   mux := http.NewServeMux()
 
   fileServer := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+  mux.HandleFunc("/", app.home)
+  mux.HandleFunc("/snippet/view", app.snippetView)
+  mux.HandleFunc("/snippet/create", app.snippetCreate)
 
   srv := &http.Server {
     Addr:     *addr,
     ErrorLog:  errorLog,
     Handler:  mux,
   }
-  infoLog.Printf("Starting server on %d", *addr)
+  infoLog.Printf("Starting server on %s", *addr)
   err := srv.ListenAndServe()
   errorLog.Fatal(err)
 }
