d529d148129b3060167e638029738d9f2898e954
turtel42 <papadimitrios216@gmail.com>
2024-04-10 00:36:44 +0300

    added MySQL connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+  "database/sql"
   "flag"
   "log"
   "net/http"
   "os"
+  
+  _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,6 +19,7 @@ func main() {
   
   // You can run go run ./cmd/web -help to get the flag info e.g. "HTTP network adress" and the conversion type e.g. String
   addr := flag.String("addr", ":4000", "HTTP network adress")
+  dsn := flag.String("dsn", "web:7777@/snippetbox?parseTime=true", "MySQL data source name")
 
   flag.Parse()
 
@@ -24,6 +28,13 @@ func main() {
 
   infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
   errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Lshortfile)
+
+  db, err := openDB(*dsn)
+  if err != nil {
+    errorLog.Fatal(err)
+  }
+
+  defer db.Close()
 
   app := &application{
     errorLog: errorLog,
@@ -36,6 +47,17 @@ func main() {
     Handler:  app.routes(),
   }
   infoLog.Printf("Starting server on %d", *addr)
-  err := srv.ListenAndServe()
+  err = srv.ListenAndServe()
   errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+    return nil, err
+  }
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
 }
