8a2852de303fbe749b9b34baf1c2c842e4a43b2a
Turtel216 <papadimitrios216@gmail.com>
2024-04-11 13:26:11 +0300

    snippets.go refactor

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -18,12 +18,12 @@ type SnippetModel struct {
   DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+func (model *SnippetModel) Insert(title string, content string, expires int) (int, error) {
 
   stmt :=  `INSERT INTO snippets (title, content, created, expires)
   VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-  result, err := m.DB.Exec(stmt, title, content, expires)
+  result, err := model.DB.Exec(stmt, title, content, expires)
   if err != nil {
     return 0, err
   }
@@ -36,12 +36,12 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
   return int(id), nil
 }
 
-func (m *SnippetModel) Get(id int) (*Snippet, error) {
+func (model *SnippetModel) Get(id int) (*Snippet, error) {
 
   stmt := `SELECT id, title, content, created, expires FROM snippets
   WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-  row := m.DB.QueryRow(stmt, id)
+  row := model.DB.QueryRow(stmt, id)
 
   s := &Snippet{}
 
@@ -58,6 +58,6 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
   return s, nil
 }
 
-func (m *SnippetModel) Latest() ([]*Snippet, error) {
+func (model *SnippetModel) Latest() ([]*Snippet, error) {
   return nil, nil
 }
