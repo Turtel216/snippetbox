62534ef5c6b8cfe0a28b3bedb74b5adeaa50e776
turtel42 <papadimitrios216@gmail.com>
2024-04-17 19:39:06 +0300

    added snippetCreatePost

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -58,15 +58,32 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request)  {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  title := "O snail"
-  content := "sdfkalsdflskdfj"
-  expires := 7
+  err := r.ParseForm()
+  if err != nil {
+    app.clientError(w, http.StatusBadRequest)
+    return
+  }
 
-  id, err := app.snippets.Insert(title, content, expires)
+  title := r.PostForm.Get("title")
+  content := r.PostForm.Get("content")
+
+  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+  if err != nil {
+    app.clientError(w, http.StatusBadRequest)
+    return
+  }
+
+  id, err := app.snippets.Insert(title, content, expires) 
   if err != nil {
     app.serverError(w, err)
-    return 
+    return
   }
 
   http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+
 }
+
+
+
+
+
