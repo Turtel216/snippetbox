b160e48295587e3c1aff5f250e0d630d48008d83
Turtel216 <papadimitrios216@gmail.com>
2024-06-13 00:40:53 +0300

    Formated files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,123 +5,117 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-  "strings"
-  "unicode/utf8"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.dimitrios_papakonstantinou.com/internal/models"
 )
 
 type snippetCreateForm struct {
-  Title         string
-  Content       string
-  Expires       int
-  FieldErrors   map[string]string
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
-  snippets, err := app.snippets.Latest()
-  if err != nil {
-    app.serverError(w, err)
-    return
-  }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippets = snippets
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-  app.render(w, http.StatusOK, "home.html", data)
+	app.render(w, http.StatusOK, "home.html", data)
 }
-
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  params := httprouter.ParamsFromContext(r.Context())
+	params := httprouter.ParamsFromContext(r.Context())
 
-  id, err := strconv.Atoi(params.ByName("id"))
+	id, err := strconv.Atoi(params.ByName("id"))
 
-  if err != nil || id < 1 {
-    app.serverError(w, err)
-    return
-  }
+	if err != nil || id < 1 {
+		app.serverError(w, err)
+		return
+	}
 
-  snippet, err := app.snippets.Get(id)
-  if err != nil {
-    if errors.Is(err, models.ErrNoRecord) {
-      app.notFound(w)
-    } else {
-      app.serverError(w, err)
-    }
-    return
-  }
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippet = snippet
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-  app.render(w, http.StatusOK, "view.html", data)
+	app.render(w, http.StatusOK, "view.html", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request)  {
-  data := app.newTemplateData(r)
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
 
-  data.Form = snippetCreateForm{
-    Expires: 365,
-  }
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 
-  app.render(w, http.StatusOK, "create.html", data)
+	app.render(w, http.StatusOK, "create.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  err := r.ParseForm()
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-  form := snippetCreateForm{
-    Title:        r.PostForm.Get("title"),
-    Content:      r.PostForm.Get("content"),
-    Expires:      expires,
-    FieldErrors:  map[string]string{},
-  }
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
 
-  if strings.TrimSpace(form.Title) == "" {
-    form.FieldErrors["title"] = "this field cannot be blank"
-  } else if utf8.RuneCountInString(form.Title) > 100 {
-    form.FieldErrors["title"] = "this field cannot be more than 100 characters long"
-  }
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "this field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "this field cannot be more than 100 characters long"
+	}
 
-  if strings.TrimSpace(form.Content) == "" {
-    form.FieldErrors["content"] = "This field cannot be blank"
-  }
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
 
-  if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-    form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-  }
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
 
-  if len(form.FieldErrors) > 0 {
-    data := app.newTemplateData(r)
-    data.Form = form 
-    app.render(w, http.StatusUnprocessableEntity, "create.html", data)
-    return
-  }
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.html", data)
+		return
+	}
 
-  id, err := app.snippets.Insert(form.Title, form.Content, form.Expires) 
-  if err != nil {
-    app.serverError(w, err)
-    return
-  }
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
-
-
-
-
-
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,74 +1,74 @@
 package main
 
 import (
-  "database/sql"
-  "flag"
-  "html/template"
-  "log"
-  "net/http"
-  "os"
+	"database/sql"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 
-  "snippetbox.dimitrios_papakonstantinou.com/internal/models"
-  
-  _ "github.com/go-sql-driver/mysql"
+	"snippetbox.dimitrios_papakonstantinou.com/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-  errorLog  *log.Logger
-  infoLog   *log.Logger
-  snippets  *models.SnippetModel
-  templateCache map[string]*template.Template
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
-  
-  // You can run go run ./cmd/web -help to get the flag info e.g. "HTTP network adress" and the conversion type e.g. String
-  addr := flag.String("addr", ":4000", "HTTP network adress")
-  dsn := flag.String("dsn", "web:7777@/snippetbox?parseTime=true", "MySQL data source name")
 
-  flag.Parse()
+	// You can run go run ./cmd/web -help to get the flag info e.g. "HTTP network adress" and the conversion type e.g. String
+	addr := flag.String("addr", ":4000", "HTTP network adress")
+	dsn := flag.String("dsn", "web:7777@/snippetbox?parseTime=true", "MySQL data source name")
 
-  // You can redirect the stdout and stderr streams to on disk files when starting the application like so 
-  // go run ./cmd/web >>/tmp/info.log 2 >>/tmp/error.log
-  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Lshortfile)
+	flag.Parse()
 
-  db, err := openDB(*dsn)
-  if err != nil {
-    errorLog.Fatal(err)
-  }
+	// You can redirect the stdout and stderr streams to on disk files when starting the application like so
+	// go run ./cmd/web >>/tmp/info.log 2 >>/tmp/error.log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Lshortfile)
 
-  defer db.Close()
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
-  templateCache, err := newTemplateCache()
-  if err != nil {
-    errorLog.Fatal(err)
-  }
-  app := &application{
-    errorLog:       errorLog,
-    infoLog:        infoLog,
-    snippets:       &models.SnippetModel{DB: db},
-    templateCache:  templateCache,
-  }
+	defer db.Close()
 
-  srv := &http.Server {
-    Addr:     *addr,
-    ErrorLog:  errorLog,
-    Handler:  app.routes(),
-  }
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	app := &application{
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
 
-  infoLog.Printf("Starting server on %s", *addr)
-  err = srv.ListenAndServe()
-  errorLog.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
-  db, err := sql.Open("mysql", dsn)
-  if err != nil {
-    return nil, err
-  }
-  if err = db.Ping(); err != nil {
-    return nil, err
-  }
-  return db, nil
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
