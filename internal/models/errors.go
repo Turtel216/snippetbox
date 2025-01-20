e98ab4809bd4d23320f220653cf7a69fadf8a16f
Turtel216 <papadimitrios216@gmail.com>
2025-01-21 00:54:41 +0200

    added user model

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -1,7 +1,13 @@
 package models
 
 import (
-  "errors"
+	"errors"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	// Snippet model
+	ErrNoRecord = errors.New("models: no matching record found")
+	// User model
+	ErrInvalidCredentails = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
