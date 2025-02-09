2690bb45412fc8e9576e610f45a599d5104a6bb8
Turtel216 <papadimitrios216@gmail.com>
2025-02-10 01:01:22 +0200

    added authorisation

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -69,7 +69,14 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
+}
+
+// isAuthenticated returns true if the currect request is from an authenticated user,
+// otherwise it returns false
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
